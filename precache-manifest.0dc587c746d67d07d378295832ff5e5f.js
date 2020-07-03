3aa5aec7a0bde97a80396fc529d0892bb69f9b5b
Raymond Findlay <raymond@automated-intelligence.com>
2020-07-03 14:59:14 +0100

    Updates

---
+++ precache-manifest.0dc587c746d67d07d378295832ff5e5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5f8e7226288d6d349260d4ec0a8bf9",
+    "url": "/jre-bingo/index.html"
+  },
+  {
+    "revision": "ef0b9446971de6543f43",
+    "url": "/jre-bingo/static/css/main.b3bd4488.chunk.css"
+  },
+  {
+    "revision": "2b010c96252f1584f85b",
+    "url": "/jre-bingo/static/js/2.30ca9ac7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jre-bingo/static/js/2.30ca9ac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0b9446971de6543f43",
+    "url": "/jre-bingo/static/js/main.bd2ddc8e.chunk.js"
+  },
+  {
+    "revision": "d4199c0846345e1cb987",
+    "url": "/jre-bingo/static/js/runtime-main.da42a842.js"
+  }
+]);
